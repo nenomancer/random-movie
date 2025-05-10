49ccde452719d9ced3bb90cb31dea5f55ad419a6
Nebojsha Kovachevikj <nebojsha.kovachevikj@valtech.com>
2025-05-10 10:27:21 +0200

    create filters store

--- vite-refactor/src/stores/movie.ts
+++ vite-refactor/src/stores/movie.ts
@@ -1,7 +1,8 @@
 import { writable } from "svelte/store";
 import { DEFAULT_MOVIE } from "../lib/constants";
-import type { Movie } from "../lib/types";
+import { type Filters, type Movie } from "../lib/types";
 
 
 export const currentMovie = writable<Movie>(DEFAULT_MOVIE);
 export const useFilters = writable<boolean>(false);
+export const currentFilters = writable<Filters>();
