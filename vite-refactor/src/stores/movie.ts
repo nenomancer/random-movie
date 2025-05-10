70ff3b5a3f26865501b2cb72a521dacadd147022
Nebojsha Kovachevikj <nebojsha.kovachevikj@valtech.com>
2025-05-10 11:28:16 +0200

    add default filter constant

--- vite-refactor/src/stores/movie.ts
+++ vite-refactor/src/stores/movie.ts
@@ -1,8 +1,8 @@
 import { writable } from "svelte/store";
-import { DEFAULT_MOVIE } from "../lib/constants";
+import { DEFAULT_FILTER, DEFAULT_MOVIE, RELEASE_YEAR_MAX, RELEASE_YEAR_MIN } from "../lib/constants";
 import { type Filters, type Movie } from "../lib/types";
 
 
 export const currentMovie = writable<Movie>(DEFAULT_MOVIE);
 export const useFilters = writable<boolean>(false);
-export const currentFilters = writable<Filters>();
+export const currentFilters = writable<Filters>(DEFAULT_FILTER);
